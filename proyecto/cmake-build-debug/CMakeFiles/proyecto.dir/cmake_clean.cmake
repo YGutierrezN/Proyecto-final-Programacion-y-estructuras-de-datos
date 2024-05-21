f667757aa7c1db9faac5e509df7b9e9a812ea669
YGutierrezN <136632782+YGutierrezN@users.noreply.github.com>
2024-05-21 10:22:29 -0500

    pro

---
+++ proyecto/cmake-build-debug/CMakeFiles/proyecto.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyecto.dir/main.cpp.obj"
+  "CMakeFiles/proyecto.dir/main.cpp.obj.d"
+  "libproyecto.dll.a"
+  "proyecto.exe"
+  "proyecto.exe.manifest"
+  "proyecto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proyecto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
